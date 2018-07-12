48ed1c31dbfa2f329b0ec79cc6ac896dc39ad90b
MichaelDracy <33749884+MichaelDracy@users.noreply.github.com>
2018-07-12 19:14:09 +0200

    Add files via upload

---
+++ view/src/main/java/view/Observer.java
@@ -0,0 +1,14 @@
+package view;
+
+/**
+ * 
+ * @author Clement Chabrier
+ *interface used to build an observer patern
+ */
+public interface Observer 
+{
+	/**
+	 * methode used to update information sent to a class
+	 */
+	public void update();
+}
